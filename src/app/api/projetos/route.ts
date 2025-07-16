9f2f6278ca60a5ea0d7ef785b339fc63ea1d2df6
“renatowillon” <renatowillon@hotmail.com>
2025-07-16 17:13:14 -0300

    fix:  delete api projetos

--- src/app/api/projetos/route.ts
+++
@@ -1,7 +0,0 @@
-// import { pegarProjetos } from "@/controllers/projectController";
-// import { NextResponse } from "next/server";
-
-// export async function GET() {
-//   const projects = await pegarProjetos();
-//   return NextResponse.json(projects);
-// }
