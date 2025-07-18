9fbb3a6bbd2afe26f34119501e6649b879d59bd2
Renato Willon <renatowillon@hotmail.com>
2025-07-17 21:45:18 -0300

    feat: endpoint de PUT Projeto

--- src/app/api/projetos/[id]/route.ts
+++ src/app/api/projetos/[id]/route.ts
@@ -1,4 +1,8 @@
-import { pegarUmProjeto } from "@/controllers/projetoController";
+import {
+  atualizarProjeto,
+  pegarUmProjeto,
+} from "@/controllers/projetoController";
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
@@ -12,4 +16,28 @@ export async function GET(
 export async function PUT(
   req: NextRequest,
   { params }: { params: { id: number } }
-) {}
+) {
+  const id = Number(params.id);
+
+  if (isNaN(id)) {
+    return NextResponse.json({ error: "ID inválido" }, { status: 400 });
+  }
+  try {
+    const data = await req.json();
+    const projetoExistente = await pegarUmProjeto(id);
+    if (!projetoExistente) {
+      return NextResponse.json(
+        { error: "Projeto não encontrado" },
+        { status: 400 }
+      );
+    }
+    const projetoAtualizado = await atualizarProjeto(id, data);
+    return NextResponse.json(projetoAtualizado, { status: 201 });
+  } catch (error) {
+    console.error("erro ao atualizar projeto:", error);
+    return NextResponse.json(
+      { error: "Erro interno ao atualizar projeto" },
+      { status: 500 }
+    );
+  }
+}
