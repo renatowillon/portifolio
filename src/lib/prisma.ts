866cfd5d5a2b3f04f10877aa4c215b84459c2bf6
Renato Willon <renatowillon@hotmail.com>
2025-08-30 15:07:22 -0300

    feat: correção do Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,35 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   var cachedPrisma: PrismaClient;
+// }
+// let prisma: PrismaClient;
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.cachedPrisma) {
+//     global.cachedPrisma = new PrismaClient();
+//   }
+//   prisma = global.cachedPrisma;
+// }
+
+// export const db = prisma;
+
 import { PrismaClient } from "@prisma/client";
 
+// Evita múltiplas instâncias do Prisma no desenvolvimento
 declare global {
-  var cachedPrisma: PrismaClient;
-}
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prisma = global.cachedPrisma;
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient | undefined;
 }
 
-export const db = prisma;
+export const db =
+  global.cachedPrisma ??
+  new PrismaClient({
+    log: ["query"], // opcional, útil para debug
+  });
+
+// Salva a instância no global no desenvolvimento
+if (process.env.NODE_ENV !== "production") {
+  global.cachedPrisma = db;
+}
